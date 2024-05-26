e4c9b29162dff1addcc41c1aeb16de1826110cd9
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-05-26 21:57:58 +0600

    header.blade.php, wishlist.blade.php and cart.blade.php Updated

--- GadgetSphere/resources/views/frontend/pages/wishlist.blade.php
+++ GadgetSphere/resources/views/frontend/pages/wishlist.blade.php
@@ -46,7 +46,7 @@
 											<p class="product-name"><a href="{{route('product-detail',$wishlist->product['slug'])}}">{{$wishlist->product['title']}}</a></p>
 											<p class="product-des">{!!($wishlist['summary']) !!}</p>
 										</td>
-										<td class="total-amount" data-title="Total"><span>${{$wishlist['amount']}}</span></td>
+										<td class="total-amount" data-title="Total"><span>BDT {{$wishlist['amount']}}</span></td>
 										<td><a href="{{route('add-to-cart',$wishlist->product['slug'])}}" class='btn text-white'>Add To Cart</a></td>
 										<td class="action" data-title="Remove"><a href="{{route('wishlist-delete',$wishlist->id)}}"><i class="ti-trash remove-icon"></i></a></td>
 									</tr>
@@ -79,7 +79,7 @@
 					<div class="single-service">
 						<i class="ti-rocket"></i>
 						<h4>Free shiping</h4>
-						<p>Orders over $100</p>
+						<p>Orders over BDT 10,000</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
